8bf976b720e1c4cebeba8d5661ffcbf610a71fa2
Diego Smania <diego.smania@gmail.com>
2023-05-02 19:15:57 -0300

    [Resources/Views]: Sidebar now uses the brand link component.

--- resources/views/components/layout/sidebar/sidebar.blade.php
+++ resources/views/components/layout/sidebar/sidebar.blade.php
@@ -1,16 +1,17 @@
 {{-- Sidebar --}}
 {{-- TODO: Make the basic sidebar markup --}}
 {{-- TODO: What properties may be add to the component? --}}
-{{-- TODO: Create a sidebar brand component! --}}
 
 <aside class="{{ $makeSidebarClasses() }}" data-bs-theme="dark">
 
     {{-- Sidebar brand --}}
-    <div class="sidebar-brand">
-        SIDEBAR BRAND
-    </div>
+    <x-ladmin-sidebar-brand label="AdminLTE"
+        logo="{{ asset('vendor/laralive-admin/img/AdminLTELogo.png') }}"
+        logo-alt="AdminLTE"
+        logo-classes="opacity-75 shadow rounded-circle"/>
 
     {{-- Sidebar menu wrapper --}}
+    {{-- TODO: Create components for sidebar items (example, link and treeview) --}}
     <div class="sidebar-wrapper">
         <nav class="mt-2">
             SIDEBAR
