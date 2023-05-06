0cfb7e41c25fb2f957f1011f66810fb8efbf0ee0
Diego Smania <diego.smania@gmail.com>
2023-05-06 11:52:09 -0300

    [Resources/Views]: Improve sidebar with link examples.

--- resources/views/components/layout/sidebar/sidebar.blade.php
+++ resources/views/components/layout/sidebar/sidebar.blade.php
@@ -15,7 +15,25 @@
     {{-- TODO: Create components for sidebar items (example, link and treeview) --}}
     <div class="sidebar-wrapper">
         <nav class="mt-2">
-            SIDEBAR
+
+            {{-- Sidebar menu --}}
+            <ul class="nav sidebar-menu flex-column"
+                data-lte-toggle="treeview"
+                data-accordion="false"
+                role="menu">
+
+                <x-ladmin-sidebar-link label="Link 1" icon="fa-regular fa-circle"/>
+
+                <x-ladmin-sidebar-link label="Link 2" icon="fa-regular fa-square" theme="warning"/>
+
+                <x-ladmin-sidebar-link label="Link 3" icon="fa-solid fa-tag"
+                    badge="5" badge-theme="info"/>
+
+                <x-ladmin-sidebar-link label="Link 4" icon="fa-solid fa-user" theme="info"
+                    badge="7" badge-theme="danger" badge-classes="rounded-circle"/>
+
+            </ul>
+
         </nav>
     </div>
 
