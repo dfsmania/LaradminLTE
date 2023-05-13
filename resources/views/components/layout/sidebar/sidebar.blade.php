385bc551c718fbc94f5efe71756944b60f27297a
Diego Smania <diego.smania@gmail.com>
2023-05-13 11:25:05 -0300

    [Resources/Views]: Improve sidebar with treeview examples.

--- resources/views/components/layout/sidebar/sidebar.blade.php
+++ resources/views/components/layout/sidebar/sidebar.blade.php
@@ -21,20 +21,42 @@
                 data-accordion="false"
                 role="menu">
 
-                <x-ladmin-sidebar-header label="Header 1" class="text-uppercase"/>
+                {{-- Basic Link tests --}}
+
+                <x-ladmin-sidebar-header label="Basic Link Tests" icon="fa-regular fa-bookmark"
+                    class="text-uppercase fw-bold" theme="success"/>
 
                 <x-ladmin-sidebar-link label="Link 1" icon="fa-regular fa-circle"/>
 
                 <x-ladmin-sidebar-link label="Link 2" icon="fa-regular fa-square" theme="warning"/>
 
-                <x-ladmin-sidebar-link label="Link 3" icon="fa-solid fa-tag"
+                <x-ladmin-sidebar-link label="Link 3" icon="fa-regular fa-circle-dot"
                     badge="5" badge-theme="info"/>
 
                 <x-ladmin-sidebar-link label="Link 4" icon="fa-solid fa-star" theme="info"
                     badge="7" badge-theme="danger" badge-classes="rounded-circle"/>
 
-                <x-ladmin-sidebar-header label="Header 2" icon="fa-regular fa-bookmark"
-                    theme="danger" class="fw-bold text-uppercase"/>
+                {{-- Treeview menu tests --}}
+
+                <x-ladmin-sidebar-header label="Treeview Menu Tests" icon="fa-regular fa-bookmark"
+                    class="text-uppercase fw-bold" theme="success"/>
+
+                <x-ladmin-sidebar-treeview label="Menu 1" icon="fa-solid fa-cubes">
+                    <x-ladmin-sidebar-link label="Link A" icon="fa-regular fa-circle"/>
+                    <x-ladmin-sidebar-link label="Link B" icon="fa-regular fa-circle"/>
+
+                    <x-ladmin-sidebar-treeview label="Menu 2" icon="fa-solid fa-cubes"
+                        theme="info" badge="2" badge-theme="warning">
+                        <x-ladmin-sidebar-link label="Link C" icon="fa-regular fa-circle"/>
+                        <x-ladmin-sidebar-link label="Link D" icon="fa-regular fa-circle"/>
+                    </x-ladmin-sidebar-treeview>
+                </x-ladmin-sidebar-treeview>
+
+                <x-ladmin-sidebar-treeview label="Menu 3" icon="fa-solid fa-cubes"
+                    theme="danger" badge="2">
+                    <x-ladmin-sidebar-link label="Link E" icon="fa-regular fa-circle"/>
+                    <x-ladmin-sidebar-link label="Link F" icon="fa-regular fa-circle"/>
+                </x-ladmin-sidebar-treeview>
 
             </ul>
 
