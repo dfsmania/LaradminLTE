2bb3fd6648e7fc75c5efc15af69ff744e84601d4
Diego Smania <diego.smania@gmail.com>
2025-04-25 19:07:17 -0300

    [src]: Fix main package class for current uploaded version

--- src/LaradminLte.php
+++ src/LaradminLte.php
@@ -8,14 +8,6 @@ use DFSmania\LaradminLte\Tools\Plugins\PluginsManager;
 class LaradminLte
 {
     /**
-     * An instance of the menu manager. This is used to read, validate, and
-     * classify the set of configured menu items.
-     *
-     * @var MenuManager
-     */
-    public MenuManager $menu;
-
-    /**
      * An instance of the plugins manager. This is used to read, validate, and
      * classify the set of configured plugins resources.
      *
@@ -30,12 +22,6 @@ class LaradminLte
      */
     public function __construct()
     {
-        // Init the menu manager with the provided menu items configuration.
-
-        $this->menu = new MenuManager(
-            is_array(config('ladmin_menu')) ? config('ladmin_menu') : []
-        );
-
         // Init the plugins manager with the provided plugins configuration.
 
         $this->plugins = new PluginsManager(
