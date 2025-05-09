9ce9128ca9fba502968eacb11b410822deb46f77
Diego Smania <diego.smania@gmail.com>
2025-05-09 16:32:52 -0300

    [src/Tools]: Improve the ResolvesMenuItemUrl trait

--- src/Tools/Menu/MenuItems/Traits/ResolvesMenuItemUrl.php
+++ src/Tools/Menu/MenuItems/Traits/ResolvesMenuItemUrl.php
@@ -19,7 +19,7 @@ trait ResolvesMenuItemUrl
         // Check if the 'url' key is defined in the configuration. If so, use
         // it to generate the URL of the item.
 
-        if (! empty($config['url']) && is_string($config['url'])) {
+        if (static::hasValidUrlKey($config)) {
             return url($config['url']);
         }
 
@@ -29,7 +29,7 @@ trait ResolvesMenuItemUrl
         // name and the second element, if present, is an array with additional
         // route parameters.
 
-        if (! empty($config['route']) && is_array($config['route'])) {
+        if (static::hasValidRouteKey($config)) {
             $routeName = $config['route'][0] ?? null;
             $routeParams = is_array($config['route'][1] ?? null)
                 ? $config['route'][1]
@@ -42,5 +42,29 @@ trait ResolvesMenuItemUrl
         // fallback placeholder URL.
 
         return '#';
+    }
+
+    /**
+     * Check if the 'url' key is defined in the configuration and is a valid
+     * string.
+     *
+     * @param  array  $config  The configuration array of the menu item
+     * @return bool
+     */
+    protected static function hasValidUrlKey(array $config): bool
+    {
+        return ! empty($config['url']) && is_string($config['url']);
+    }
+
+    /**
+     * Check if the 'route' key is defined in the configuration and is a valid
+     * array.
+     *
+     * @param  array  $config  The configuration array of the menu item
+     * @return bool
+     */
+    protected static function hasValidRouteKey(array $config): bool
+    {
+        return ! empty($config['route']) && is_array($config['route']);
     }
 }
