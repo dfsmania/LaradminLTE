19efe61682c1f18580815cc19022973f0d8e7203
Diego Smania <diego.smania@gmail.com>
2025-05-12 13:20:21 -0300

    [resources/views]: Fix markup on dropdown header menu item component

--- resources/views/components/layout/navbar/dropdown-header.blade.php
+++ resources/views/components/layout/navbar/dropdown-header.blade.php
@@ -1,12 +1,14 @@
 {{-- Navbar dropdown header --}}
-<li {{ $attributes->merge(['class' => $headerClasses]) }}>
+<li>
+    <h6 {{ $attributes->merge(['class' => $headerClasses]) }}>
 
-    {{-- Icon (optional) --}}
-    @if(! empty($icon))
-        <i class="{{ $icon }} me-1"></i>
-    @endif
+        {{-- Icon (optional) --}}
+        @if(! empty($icon))
+            <i class="{{ $icon }} me-1"></i>
+        @endif
 
-    {{-- Label --}}
-    <span>{{ $label }}</span>
+        {{-- Label --}}
+        <span>{{ $label }}</span>
 
+    </h6>
 </li>
